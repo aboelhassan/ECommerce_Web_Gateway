3abd95cdcbe92c3d0186f1b54c273aa37bd66867
aboelhassan <a.elrian@gmail.com>
2023-11-20 19:01:38 +0200

    security pom yml

--- WebGateay/src/main/java/org/ecommerce/webgateway/WebGatewayApplication.java
+++ WebGateay/src/main/java/org/ecommerce/webgateway/WebGatewayApplication.java
@@ -2,8 +2,10 @@ package org.ecommerce.webgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebGatewayApplication {
 
 	public static void main(String[] args) {
